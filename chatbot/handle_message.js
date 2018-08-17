4e93865b91a606f694dcab7a4e0dacb6f90a7b04
SkynI25 <rlxo1651@daum.net>
2018-08-17 19:22:05 +0900

    no message

--- chatbot/handle_message.js
+++ chatbot/handle_message.js
@@ -6,17 +6,41 @@ const handleMessage = function(sender_psid, received_message) {
     // Checks if the message contains text
     if (received_message.text) {
       
-      // Creates the payload for a basic text message, which
-      // will be added to the body of our request to the Send API
-      response = {
-        "text": `당신이 보낸 메시지: "${received_message.text}". !` // 사용자에게 보내는 메시지, 추후 자연어 처리(NLP)를 통해 제어문으로 어떻게 해줄지 선택하면 됨.
+      if (received_message.text === "test") {
+        response = {
+          "attachment": {
+            "type": "template",
+            "payload": {
+              "template_type":"button",
+              "text":"test를 예약하시겠습니까?",
+              "buttons":[
+                {
+                  "type": "postback",
+                  "title": "예",
+                  "payload": `{"element": "test", "choise": "yes"}`
+                },
+                {
+                  "type": "postback",
+                  "title": "아니요",
+                  "payload": `{"element": "test", "choise": "no"}`
+                }
+              ]
+            }
+          }
+        }
+      } else {
+        // Creates the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
+        response = {
+          "text": `당신이 보낸 메시지: "${received_message.text}"` // 사용자에게 보내는 메시지, 추후 자연어 처리(NLP)를 통해 제어문으로 어떻게 해줄지 선택하면 됨.
+        }
       }
   
     } else if (received_message.attachments) {
     
       // Gets the URL of the message attachment
       let attachment_url = received_message.attachments[0].payload.url;
-    
+
     } 
     
     // Sends the response message
--- chatbot/handle_postback.js
+++ chatbot/handle_postback.js
@@ -1,5 +1,6 @@
 const handlePostback = function(psid, payload) { // 이미지 밑에 버튼,이 있고 이를 선택할지 말지를 처리하는 부분
-    
+    console.log(psid)
+    console.log(JSON.stringify(payload, null, 4))
 }
 
 module.exports = handlePostback;
