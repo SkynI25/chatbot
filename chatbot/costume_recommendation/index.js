1f470cc4a6173f098bff1b531493101015fa6131
SkynI25 <rlxo1651@daum.net>
2018-08-17 21:31:25 +0900

    no message

--- chatbot/costume_recommendation/index.js
+++ chatbot/costume_recommendation/index.js
@@ -4,22 +4,22 @@ const skirt = require("./skirt");
 e = function (payload) {
   let response;
   if (payload.length === 1) {
-    if (payload[payload.length - 1] === "의상추천") {
+    if (payload[payload.length - 1] === "a") {
       response = {
         "attachment": {
           "type": "template",
           "payload": {
             "template_type": "button",
-            "text": "어떤 의상을 추천해드릴까요?",
+            "text": "밝은 계열이 좋으세요, 어두운 계열이 좋으세요?",
             "buttons": [{
                 "type": "postback",
-                "title": "치마",
-                "payload": `["의상추천", "치마"]`
+                "title": "밝은 계열",
+                "payload": ["의상추천", "a", "y"] 
               },
               {
                 "type": "postback",
-                "title": "바지",
-                "payload": `["의상추천", "바지"]`
+                "title": "어두운 계열",
+                "payload": ["의상추천", "a", "n"]
               }
             ]
           }
--- chatbot/handle_message.js
+++ chatbot/handle_message.js
@@ -12,17 +12,27 @@ const handleMessage = function(sender_psid, received_message) {
             "type": "template",
             "payload": {
               "template_type":"button",
-              "text":"어떤 의상을 추천해드릴까요?",
+              "text":"어떤 옷을 추천해드릴까요?",
               "buttons":[
                 {
                   "type": "postback",
-                  "title": "치마",
-                  "payload": `["의상추천", "치마"]`
+                  "title": "회사에서 입을 옷",
+                  "payload": ["의상추천", "a"]
                 },
                 {
                   "type": "postback",
-                  "title": "바지",
-                  "payload": `["의상추천", "바지"]`
+                  "title": "소개팅할때",
+                  "payload": ["의상추천", "b"]
+                },
+                {
+                  "type": "postback",
+                  "title": "데이트할때",
+                  "payload": ["의상추천", "c"]
+                },
+                {
+                  "type": "postback",
+                  "title": "일상에서 편하게",
+                  "payload": ["의상추천", "d"]
                 }
               ]
             }
