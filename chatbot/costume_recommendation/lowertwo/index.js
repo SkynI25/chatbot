6469a033730648d134910de320e4d646e1fbdc9e
SkynI25 <rlxo1651@daum.net>
2018-08-18 00:30:04 +0900

    no message

---
+++ chatbot/costume_recommendation/lowertwo/index.js
@@ -0,0 +1,11 @@
+const callSendAPI = require('../../call_send_api');
+const e = function (psid, payload) {
+  let response;
+  if (payload.length === 4) {
+    response = "당신에게 가장 적합한 옷을 찾는 중입니다.\n" + "잠시만 기다려주세요."
+      callSendAPI(psid, response);
+  }
+  return response;
+}
+
+module.exports = e;
