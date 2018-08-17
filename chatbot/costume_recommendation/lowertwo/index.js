0e1d0b72989521fc3e57e03e6508a28dc905dc68
SkynI25 <rlxo1651@daum.net>
2018-08-18 01:33:58 +0900

    no message

--- chatbot/costume_recommendation/lowertwo/index.js
+++ chatbot/costume_recommendation/lowertwo/index.js
@@ -1,7 +1,7 @@
 const callSendAPI = require('../../call_send_api');
 const e = function (psid, payload) {
   let response;
-  response = {"message" : "당신에게 가장 적합한 옷을 찾는 중입니다.\n 잠시만 기다려주세요."};
+  response = { "text": `당신에게 가장 적합한 옷을 찾는 중입니다.\n + 잠시만 기다려주세요.`};
   callSendAPI(psid, response);
   return response;
 }
