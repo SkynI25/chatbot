11cb20c7c1d18c6015f4ba0ab5ca2840ce26f34c
SkynI25 <rlxo1651@daum.net>
2018-08-18 00:16:58 +0900

    no message

--- chatbot/costume_recommendation/point/index.js
+++ chatbot/costume_recommendation/point/index.js
@@ -1,17 +1,8 @@
 const callSendAPI = require('../../call_send_api');
-const plain = require("./plain");
-const point = require("./point");
-
 const e = function (psid, payload) {
   let response;
   if (payload.length === 2) {
-    if (payload[payload.length - 1] === "n") {
-      
-      let aaa = JSON.parse(JSON.stringify(payload))
-      aaa.push("a")
-      let bbb = JSON.parse(JSON.stringify(payload))
-      bbb.push("b")
-      
+    if (payload[payload.length - 1] === "y") {
       response = {
         "attachment": {
           "type": "template",
@@ -21,12 +12,12 @@ const e = function (psid, payload) {
             "buttons": [{
                 "type": "postback",
                 "title": "무난한 옷",
-                "payload": JSON.stringify(aaa)
+                "payload": `["a", "y", "a"]`
               },
               {
                 "type": "postback",
                 "title": "포인트 있는 옷",
-                "payload": JSON.stringify(bbb)
+                "payload": `["a", "y", "b"]`
               }
             ]
           }
@@ -34,11 +25,6 @@ const e = function (psid, payload) {
       }
       callSendAPI(psid, response);
     }
-  } else {
-    response = point(psid, payload);
-      if (!response) {
-        response = plain(psid, payload);
-      }
   }
   return response;
 }
--- chatbot/costume_recommendation/white/index.js
+++ chatbot/costume_recommendation/white/index.js
@@ -1,4 +1,7 @@
 const callSendAPI = require('../../call_send_api');
+const plain = require("./plain");
+const point = require("./point");
+
 const e = function (psid, payload) {
   let response;
   if (payload.length === 2) {
@@ -8,16 +11,16 @@ const e = function (psid, payload) {
           "type": "template",
           "payload": {
             "template_type": "button",
-            "text": "어떤 의상을 추천해드릴까요?",
+            "text": "무난한 옷이 좋으세요, 포인트 있는 옷이 좋으세요?",
             "buttons": [{
                 "type": "postback",
-                "title": "반바지",
-                "payload": `["의상추천", "바지", "반바지"]`
+                "title": "무난한 옷",
+                "payload": `["a", "y", "a"]`
               },
               {
                 "type": "postback",
-                "title": "긴바지",
-                "payload": `["의상추천", "바지", "긴바지"]`
+                "title": "포인트 있는 옷",
+                "payload": `["a", "y", "b"]`
               }
             ]
           }
@@ -25,6 +28,11 @@ const e = function (psid, payload) {
       }
       callSendAPI(psid, response);
     }
+  } else {
+    response = point(psid, payload);
+      if (!response) {
+        response = plain(psid, payload);
+      }
   }
   return response;
 }
