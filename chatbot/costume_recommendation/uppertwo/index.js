a1a71a0f9d2fc770cd76acc55966c24ffaa3b41c
SkynI25 <rlxo1651@daum.net>
2018-08-18 04:41:01 +0900

    no message

--- chatbot/costume_recommendation/uppertwo/index.js
+++ chatbot/costume_recommendation/uppertwo/index.js
@@ -1,10 +1,6 @@
 const callSendAPI = require('../../call_send_api');
 const e = function (psid, payload) {
     let response;
-    response = { "text": `당신에게 가장 적합한 옷을 찾는 중입니다.\n잠시만 기다려주세요.`};
-    callSendAPI(psid, response);
-
-    console.log(payload);
     return response;
 }
 
